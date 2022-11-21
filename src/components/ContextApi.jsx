a39e2071016bcd1ef942d402ea41e4ccb812bcfd
Deepak <dgusaiwal537@gmail.com>
2022-11-21 21:28:59 +0530

    updating Context Folder

--- src/components/ContextApi.jsx
+++ src/components/ContextApi.jsx
@@ -2,10 +2,20 @@ import React, { useContext } from 'react'
 import ContextFile from '../context/context'
 
 const ContextApi = () => {
-    let {name} = useContext(ContextFile)
-    console.log(name)
+  let { count, dispatchofContext } = useContext(ContextFile)
+  // console.log(count, dispatchofContext)
   return (
-    <div>ContextApi {name}</div>
+    <>
+      <h1>ContextApi </h1>
+
+      <div style={{ display: "flex", gap: "2rem", margin: "1rem 0" }}>
+
+        <button onClick={() => { dispatchofContext({ type: "dec" }) }}>-</button>
+        <p>Value {count} </p>
+
+        <button onClick={() => { dispatchofContext({ type: "inc" }) }}>+</button>
+      </div>
+    </>
   )
 }
 
--- src/context/CountState.js
+++ src/context/CountState.js
@@ -1,7 +1,20 @@
+import { useReducer } from "react";
 import ContextFile from "./context";
+
+
+const reducer = (state,action)=>{
+    if(action.type==="inc"){
+        state +=1
+    }else if(action.type === 'dec'){
+        state -=1
+    }
+    return state
+}
+
  const CountState = (props)=>{
+    const [state,dispatch] = useReducer(reducer,0)
     return (
-        <ContextFile.Provider value={{name:"Deepak"}}>
+        <ContextFile.Provider value={{count:state,dispatchofContext:dispatch}}>
            {props.children}
         </ContextFile.Provider>
     )
